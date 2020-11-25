7e5e15add644a3427aaa5d29c846972911b63ac3
Zhang <zhany016@newschool.edu>
2020-11-25 14:12:34 -0500

    first init

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { title } = require("process");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  devServer: {
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/template.html",
+      title: "New template",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
